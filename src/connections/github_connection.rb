d99e37680a7e5da022f0c8c86a6a7b844ff4fbe2
hkdnet <hkdnet@users.noreply.github.com>
2015-10-07 09:35:38 +0900

    add github connection

---
+++ src/connections/github_connection.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require 'json'
+
+module GitRec
+  module Connections
+    class GitHubConnection
+      def user(name)
+        fetch("/users/#{name}")
+      end
+
+      def commits(owner, repo)
+        fetch("/repos/#{owner}/#{repo}/commits")
+      end
+
+      def fetch(path)
+        res = open(base_url + path)
+        code, message = res.status # res.status => ["200", "OK"]
+
+        if code != '200'
+          puts "OMG!! #{code} #{message}"
+          return
+        end
+        result = res.read
+        JSON[result]
+      end
+
+      def base_url
+        'https://api.github.com'
+      end
+    end
+  end
+end
