da5263ca3e1a8d66cbdd948c6fa6d6151e595d07
hkdnet <hkdnet@users.noreply.github.com>
2015-10-08 00:07:12 +0900

    access to repository history

--- src/connections/github_connection.rb
+++ src/connections/github_connection.rb
@@ -9,24 +9,35 @@ module GitRec
         fetch("/users/#{name}")
       end
 
-      def commits(owner, repo)
-        fetch("/repos/#{owner}/#{repo}/commits")
+      def commits(owner, repo, author: nil, date: nil)
+        url = "/repos/#{owner}/#{repo}/commits?" \
+              "#{author_query(author)}" \
+              "#{date_query(date)}"
+        fetch(url)
       end
+
+      private
 
       def fetch(path)
         res = open(base_url + path)
         code, message = res.status # res.status => ["200", "OK"]
-
-        if code != '200'
-          puts "OMG!! #{code} #{message}"
-          return
-        end
-        result = res.read
-        JSON[result]
+        fail "OMG!! #{code} #{message}" if code != '200'
+        JSON[res.read]
       end
 
       def base_url
         'https://api.github.com'
+      end
+
+      def author_query(author)
+        author.nil? ? '' : "author=#{author}&"
+      end
+
+      def date_query(date)
+        date = Date.today unless date
+        since_date = date.to_datetime.iso8601
+        until_date = (date + 1).to_datetime.iso8601
+        since.nil? ? '' : "since=#{since_date}&until=#{until_date}"
       end
     end
   end
