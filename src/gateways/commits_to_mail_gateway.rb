75fb664c7462d314b891f16445c8eb0de658ac61
hkdnet <hkdnet@users.noreply.github.com>
2015-10-10 18:54:13 +0900

    HTMLメール化(MIMEがちょっとアレ)

--- src/gateways/commits_to_mail_gateway.rb
+++ src/gateways/commits_to_mail_gateway.rb
@@ -9,15 +9,19 @@ module GitRec
       end
 
       def mail(from, to)
-        messages = @commits.map do |e|
-          [e.url, e.commit.message].join("\n")
-        end
+        messages = @commits.map { |e| map_to_mail(e) }
         message = messages.join("\n")
         GitRec::Models::Mail.new.tap do |m|
           m.from = from
           m.to = to
           m.message = message
         end
+      end
+
+      def map_to_mail(e)
+        "* #{e.commit.message.split("\n").select{ |l| !l.empty? }.map do |line|
+          "[#{line}](#{e.url})"
+        end.join("\n")}\n"
       end
     end
   end
--- src/models/mail.rb
+++ src/models/mail.rb
@@ -1,4 +1,5 @@
 require 'nkf'
+require 'redcarpet'
 
 module GitRec
   module Models
@@ -8,26 +9,38 @@ module GitRec
 
       # rubocop:disable MethodLength
       def raw_text
-        <<-END
+        md = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+        <<-EOS
 From: GitRecReport <#{@from}>
 To: いつもがんばってる君へ <#{@to}>
 Subject: #{NKF.nkf('-jWM', '[GitRec]がんばりレポート')}
 Date: #{Time.now.strftime('%a, %d %b %Y %X %z')}
 Mime-Version: 1.0
-Content-Type: text/plain; charset=ISO-2022-JP
+Content-Type: text/html; charset=ISO-2022-JP
 Content-Transfer-Encoding: 7bit
 
-こんにちは、元気してる？今週のレポートだよ。
+<html>
+<body>
+#{md.render(markdown_message)}
+</body>
+</html>
+EOS
+      end
+      # rubocop:enable MethodLength
+
+      def markdown_message
+        <<-EOS
+## 今週のレポートだよ。
 
 ----
+
 #{@message}
+
 ----
 
 以上だよ。来週もまたがんばろうね！
-
-END
+EOS
       end
-      # rubocop:enable MethodLength
     end
   end
 end
