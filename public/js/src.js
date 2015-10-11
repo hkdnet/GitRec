20e52dc6c7dad67a392c73f20fd2072a85992569
hkdnet <hkdnet@users.noreply.github.com>
2015-10-12 03:40:06 +0900

    日付入力項目2つ追加

--- public/js/src.js
+++ public/js/src.js
@@ -1,15 +1,20 @@
+'use strict';
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Button } from 'react-bootstrap';
 import toastr from 'toastr';
 import req from 'superagent-bluebird-promise';
+import dateFormat from 'dateformat'
 
 class Screen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       owner: '',
-      repo: ''
+      repo: '',
+      sinceDate: dateFormat(new Date(), 'yyyy-mm-dd'),
+      untilDate: dateFormat(new Date(), 'yyyy-mm-dd')
     };
   }
   sendButtonClickHandler() {
@@ -19,9 +24,11 @@ class Screen extends React.Component {
       return false;
     }
     let url = '/mail/' + this.state.owner + '/' + this.state.repo;
+    toastr.info('sending...', 'INFO');
     req.get(url)
        .send()
        .then(() =>{
+         toastr.clear();
          toastr.info('GitRec report was successfully sent', 'GitRec report');
        });
   }
@@ -30,6 +37,12 @@ class Screen extends React.Component {
   }
   repoChangeHandler(e) {
     this.setState({ repo: e.target.value });
+  }
+  sinceDateChangeHandler(e) {
+    this.setState({ sinceDate: e.target.value })
+  }
+  untilDateChangeHandler(e) {
+    this.setState({ untilDate: e.target.value })
   }
   render() {
     return (
@@ -43,6 +56,15 @@ class Screen extends React.Component {
           <input id="repo" defaultValue={this.state.repo}
             onChange={this.repoChangeHandler.bind(this)}
             placeholder="repo" />
+        </div>
+        <div>
+          <input id="since_date" type="date"
+            defaultValue={this.state.sinceDate}
+            onChange={this.sinceDateChangeHandler.bind(this)} />
+          <span>〜</span>
+          <input id="until_date" type="date"
+            defaultValue={this.state.untilDate}
+            onChange={this.untilDateChangeHandler.bind(this)} />
         </div>
         <Button id="send-mail" onClick={this.sendButtonClickHandler.bind(this)}>
           send GitRec report
