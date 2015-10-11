5db55f0807ed12658ef44c4fb9cb51f147b6b950
hkdnet <hkdnet@users.noreply.github.com>
2015-10-12 02:04:00 +0900

    V-DOM更新

--- public/js/src.js
+++ public/js/src.js
@@ -3,15 +3,34 @@ import ReactDom from 'react-dom';
 import { Button } from 'react-bootstrap';
 
 class Screen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      owner: '',
+      repo: ''
+    };
+  }
   sendButtonClickHandler() {
-    console.log('hello')
+    console.log(this.state.owner, this.state.repo)
+  }
+  ownerChangeHandler(e) {
+    this.setState({ owner: e.target.value });
+  }
+  repoChangeHandler(e) {
+    this.setState({ repo: e.target.value });
   }
   render() {
     return (
       <div className="screen">
         <h1 className="title">GitRec</h1>
-        <input id="owner" /> / <input id="repo" /><br />
-        <Button id="send-mail" onClick={this.sendButtonClickHandler}>
+        <div>
+          <input id="owner" defaultValue={this.state.owner}
+            onChange={this.ownerChangeHandler.bind(this)} />
+          <span>/</span>
+          <input id="repo" defaultValue={this.state.repo}
+            onChange={this.repoChangeHandler.bind(this)} />
+        </div>
+        <Button id="send-mail" onClick={this.sendButtonClickHandler.bind(this)}>
           send GitRec report
         </Button>
       </div>
