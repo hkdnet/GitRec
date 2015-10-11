19bdc757def3bc160a7f0948cd6816f01a8bddd9
hkdnet <hkdnet@users.noreply.github.com>
2015-10-11 19:27:47 +0900

    React bootstrap化

--- public/js/src.js
+++ public/js/src.js
@@ -1,10 +1,19 @@
-const React = require('react');
-const ReactDom = require('react-dom');
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Button } from 'react-bootstrap';
 
 class Screen extends React.Component {
+  sendButtonClickHandler() {
+    console.log('hello')
+  }
   render() {
     return (
       <div className="screen">
+        <h1 className="title">GitRec</h1>
+        <input id="owner" /> / <input id="repo" /><br />
+        <Button id="send-mail" onClick={this.sendButtonClickHandler}>
+          send GitRec report
+        </Button>
       </div>
     );
   }
@@ -13,3 +22,21 @@ ReactDom.render(
   <Screen />,
   document.getElementById('content')
 );
+
+/*
+var sendMailButton = document.getElementById('send-mail');
+var valGetter = function(input) {
+  return input && input.value;
+};
+sendMailButton.addEventListener('click', function(e) {
+  var owner = valGetter(document.getElementById('owner')),
+      repo = valGetter(document.getElementById('repo'));
+
+  if(!owner || !repo) {
+    alert('please fill your information.')
+    return false;
+  }
+  e.target.innerText = 'sending...'
+  location.href = '/mail/' + owner + '/' + repo;
+});
+*/
