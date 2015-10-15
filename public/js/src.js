008be011992b9d952d6ba44c5c5eb920773e0fbb
hkdnet <hkdnet@users.noreply.github.com>
2015-10-16 08:31:28 +0900

    searchfilterをモジュール化

--- public/js/src.js
+++ public/js/src.js
@@ -6,43 +6,10 @@ import { Button, Grid, Row, Col, Label } from 'react-bootstrap';
 import toastr from 'toastr';
 import dateFormat from 'dateformat';
 import $ from 'jquery'
-import GitHubRepository from './public/js/gitHubRepository.js'
 
-class SearchFilter extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Grid>
-        <Row>
-          <Col xs={3}>
-            <Label>
-              フィルタ
-            </Label>
-          </Col>
-          <Col xs={9}>
-            <input id="since_date" type="date"
-              defaultValue={this.props.sinceDate}
-              onChange={this.props.sinceDateChangeHandler} />
-            <span>〜</span>
-            <input id="until_date" type="date"
-              defaultValue={this.props.untilDate}
-              onChange={this.props.untilDateChangeHandler} />
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={9} xsOffset={3}>
-            <input id="commiter"
-              placeholder="commiter(optional)"
-              defaultValue={this.props.commiter}
-              onChange={this.props.commiterChangeHandler.bind(this)} />
-          </Col>
-        </Row>
-      </Grid>
-    )
-  }
-}
+import GitHubRepository from './public/js/gitHubRepository.js'
+import SearchFilter from './public/js/searchFilter.js'
+
 class Screen extends React.Component {
   constructor(props) {
     super(props);
